471288436ce4077cdce2dd6002901a8329e75c66
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-10-28 11:12:49 +0800

    add start script

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = "Something went wrong";
   res.status(statusCode).render("error", { err });
 });
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
