1a0b4252952199a1ab9fd18b572ee1cf9a260b30
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2023-01-04 14:38:47 +0800

    deployment on Railway

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Review = require("../models/review");
-const Campground = require("../models/camp");
 const catchAsync = require("../utils/catchAsync");
 const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
